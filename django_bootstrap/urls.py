2235402375b8596205321f8fc1dcf14cc506c418
soroushkhosravi <soroushgol2022@gmail.com>
2023-09-16 11:40:10 +0100

    polls app is added.

--- django_bootstrap/urls.py
+++ django_bootstrap/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.contrib import admin
+from django.urls import include, path
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
 ]
