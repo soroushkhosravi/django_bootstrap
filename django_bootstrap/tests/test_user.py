298dcebcb2b41367d3208201856aa3aea4c41f49
soroushkhosravi <soroushgol2022@gmail.com>
2023-09-18 09:50:16 +0100

    The tests for the user model is added.

--- django_bootstrap/tests/test_user.py
+++ django_bootstrap/tests/test_user.py
@@ -10,6 +10,17 @@ def test_user_create():
   User.objects.create_user('john', 'lennon@thebeatles.com', 'johnpassword')
   assert User.objects.count() == 1
 
+  users = User.objects.all()
+
+  assert len(users) == 1
+
+  assert users[0].id == 1
+  assert users[0].email == 'lennon@thebeatles.com'
+  assert isinstance(users[0], User)
+  assert users[0].username == 'john'
+  assert users[0].first_name == ''
+  assert users[0].last_name == ''
+
 @pytest.mark.django_db
 def test_question_create():
   get_question_repo().add_question()
