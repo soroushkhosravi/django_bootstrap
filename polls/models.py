f75de802cf4c00c4f0ff6d72f4a1f5b16883eece
soroushkhosravi <soroushgol2022@gmail.com>
2023-09-18 16:04:04 +0100

    The question and choices are added.

--- polls/models.py
+++ polls/models.py
@@ -1,9 +1,33 @@
 from django.db import models
+from datetime import datetime
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
+
+    def choices_with_specific_votes(self, number_of_votes: int):
+        """Returns all the chices with 5 votes."""
+        return self.choices.filter(votes=number_of_votes)
+
+    @classmethod
+    def add_with_choices(
+            cls,
+            text: str,
+            pub_date: datetime,
+            choices: list
+    ):
+        """Adds a question with choices."""
+        question = cls(question_text=text, pub_date=pub_date)
+        question.save()
+
+        for choice_text in choices:
+            choice = Choice(
+                question=question,
+                choice_text=choice_text
+            )
+            choice.save()
+
 
 
 class Choice(models.Model):
--- tests/models/test_question.py
+++ tests/models/test_question.py
@@ -1,5 +1,6 @@
 """Tests related to the question model."""
 import pytest
+from django.db import transaction
 
 from polls.models import Question, Choice
 from datetime import datetime
@@ -39,4 +40,70 @@ def test_question_service():
     not_existing_choice = question.choices.filter(choice_text="This is not choice text.")
 
     assert len(not_existing_choice) ==0
+
+
+@pytest.mark.django_db(reset_sequences=True)
+def test_choices_with_five_votes():
+    """Tests the question repository."""
+    question = Question(
+      question_text="This is a question.",
+      pub_date=datetime.now()
+    )
+
+    assert isinstance(question, Question)
+    question.save()
+
+    choice = Choice(
+      choice_text="This is choice text.",
+      votes=10,
+      question=question
+    )
+    choice.save()
+
+    choice = Choice(
+      choice_text="This is choice text.",
+      votes=5,
+      question=question
+    )
+    choice.save()
+    assert len(question.choices.all()) == 2
+
+    assert question.id == 1
+    assert question.question_text == "This is a question."
+
+    choices_with_five_votes = (
+        question.choices_with_specific_votes(number_of_votes=5)
+    )
+
+    assert len(choices_with_five_votes) == 1
+
+    choice_with_five_vote = choices_with_five_votes[0]
+
+    assert choice_with_five_vote.id == 2
+    assert choice_with_five_vote.votes == 5
+
+@pytest.mark.django_db(reset_sequences=True)
+def test_add_with_choices():
+    """Tests the add_with_choices function."""
+    Question.add_with_choices(
+        text="This is a question.",
+        pub_date=datetime.now(),
+        choices=["This is choice one.", "This is choice two."]
+    )
+
+    questions = Question.objects.all()
+
+    assert len(questions) == 1
+    assert questions[0].id == 1
+
+    assert len(questions[0].choices.all()) == 2
+
+    assert questions[0].choices.all()[0].id == 1
+    assert questions[0].choices.all()[0].choice_text == (
+        "This is choice one."
+    )
+    assert questions[0].choices.all()[1].choice_text == (
+        "This is choice two."
+    )
+
 
