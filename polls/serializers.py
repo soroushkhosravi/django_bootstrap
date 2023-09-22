60b1722c21c58a76655decda19906488dc38cc26
soroushkhosravi <soroushgol2022@gmail.com>
2023-09-22 16:08:51 +0100

    user serializer added and tested.

---
+++ polls/serializers.py
@@ -0,0 +1,17 @@
+"""Covers all the serializers for models."""
+from django.utils import timezone
+from rest_framework import serializers
+from rest_framework.exceptions import ErrorDetail, ValidationError
+
+from .models import Question
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    """The question serializer."""
+    id = serializers.IntegerField(read_only=True)
+    question_text = serializers.CharField(max_length=200)
+    pub_date = serializers.DateTimeField("date published")
+
+    class Meta:
+        model = Question
+        fields = ("id", "question_text", "pub_date")
---
+++ tests/serializers/test_question_serializer.py
@@ -0,0 +1,38 @@
+"""Tests related to the question serializer."""
+import pytest
+from collections import OrderedDict
+from datetime import datetime
+from django.utils import timezone
+from zoneinfo import ZoneInfo
+
+from polls.serializers import QuestionSerializer
+
+
+@pytest.mark.parametrize(
+    "data, is_valid, validated_data",
+    [
+        (
+            {"question_text": "abc", "pub_date": timezone.datetime(year=2000, month=10, day=5)},
+            True,
+            OrderedDict(
+                [
+                    ("question_text", "abc"),
+                    ("pub_date", datetime(year=2000, month=10, day=5, tzinfo=ZoneInfo(key="UTC")))])
+        ),
+        (
+            {"pub_date": timezone.now()},
+            False,
+            {}
+        ),
+        (
+            {"question_text": "abc", "pub_date": 100},
+            False,
+            {}
+        )
+    ]
+)
+def test_serializer_validates_data_as_expected(data, is_valid, validated_data):
+    """Tests we can validate data passed to serializer."""
+    serializer = QuestionSerializer(data=data)
+    assert serializer.is_valid() is is_valid
+    assert serializer.validated_data == validated_data
