fcbed35d9ad0e3d532b0e347fa46436ae4cf28b2
soroushkhosravi <soroushgol2022@gmail.com>
2023-09-23 10:24:43 +0100

    the fields is changed to all for question serializer.

--- polls/serializers.py
+++ polls/serializers.py
@@ -14,4 +14,5 @@ class QuestionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Question
-        fields = ("id", "question_text", "pub_date")
+        # fields = ("id", "question_text", "pub_date")
+        fields = '__all__'
