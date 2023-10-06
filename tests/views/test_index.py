03096da4d6ba1a043f18f1ebc635dafe18d95dc8
soroushkhosravi <soroushgol2022@gmail.com>
2023-10-06 16:15:20 +0100

    The test for updating questions is added.

--- tests/views/test_index.py
+++ tests/views/test_index.py
@@ -4,6 +4,7 @@ from django.test import Client
 from polls.models import Question, Choice
 from datetime import datetime
 from freezegun import freeze_time
+import json
 
 
 @pytest.mark.django_db(reset_sequences=True)
@@ -50,3 +51,77 @@ def test_get_question_for_valid_id():
         'pub_date': '2020-10-10T00:00:00Z',
         'choices': [{'choice_text': 'Choice 1', 'id': 1, 'votes': 10}]
     }
+
+
+@freeze_time("2020-10-10")
+@pytest.mark.django_db(reset_sequences=True)
+def test_updated_for_valid_data():
+    """Tests getting a question."""
+    question = Question.objects.create(
+        question_text="First question.",
+        pub_date=datetime.now()
+    )
+
+    Choice.objects.create(question=question, choice_text="Choice 1", votes=10)
+
+    client = Client()
+
+    valid_data = {
+            "question_text": "changed",
+            "pub_date": "2020-10-15",
+            "choices": [
+                {
+                    "id": 1,
+                    "choice_text": "choice 1 changed"
+                },
+                {
+                    "choice_text": "new choice"
+                }
+            ]
+        }
+
+    response = client.put('/polls/question/1', json.dumps(valid_data))
+
+    assert response.status_code == 200
+    assert response.json() == {
+        'id': 1,
+        'question_text': 'changed',
+        'pub_date': '2020-10-15T00:00:00Z',
+        'choices': [
+            {'choice_text': 'choice 1 changed', 'id': 1, 'votes': 10},
+            {'choice_text': 'new choice', 'id': 2, 'votes': 0}
+        ]
+    }
+
+
+@freeze_time("2020-10-10")
+@pytest.mark.django_db(reset_sequences=True)
+def test_updated_for_invalid_data():
+    """Tests getting a question."""
+    question = Question.objects.create(
+        question_text="First question.",
+        pub_date=datetime.now()
+    )
+
+    Choice.objects.create(question=question, choice_text="Choice 1", votes=10)
+
+    client = Client()
+
+    valid_data = {
+            "question_text": "changed",
+            "pub_date": "2020-10-15",
+            "choices": [
+                {
+                    "id": 10,
+                    "choice_text": "choice 1 changed"
+                },
+                {
+                    "choice_text": "new choice"
+                }
+            ]
+        }
+
+    response = client.put('/polls/question/1', json.dumps(valid_data))
+
+    assert response.status_code == 400
+    assert response.json() == {"message": "Choice matching query does not exist.", "status": "error"}
